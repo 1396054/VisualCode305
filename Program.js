6291fa61742010a6a592f95e16d7b1b6f9c1a27c
Jack <jgallo@linfied.edu>
2025-04-08 10:52:23 -0700

    Extracted the switch function: Highlight what you want in your new function. Refactor it and then extract it to global scope.

--- Program.js
+++ Program.js
@@ -25,7 +25,7 @@ let invoices =
 }
 
 function statement(invoices, plays) {
-    let totalAmount = 0;
+    let thisAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoices.customer}\n`;
     const format = new Intl.NumberFormat('en-US', {
@@ -36,23 +36,7 @@ function statement(invoices, plays) {
     for (let perf of invoices.performances) {
       const play = plays[perf.playID];
       let thisAmount = 0;
-      switch (play.type) {
-        case 'tragedy':
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
-          break;
-        case 'comedy':
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
-          break;
-        default:
-          throw new Error(`unknown type: ${play.type}`);
-      }
+      thisAmount = amountFor(play, thisAmount, perf);
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
@@ -61,11 +45,32 @@ function statement(invoices, plays) {
       result += ` ${play.name}: ${format(thisAmount / 100)} (${
         perf.audience
       } seats)\n`;
-      totalAmount += thisAmount;
+      thisAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${format(thisAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
   }
 
   console.log(statement(invoices, plays))
+
+function amountFor(play, thisAmount, perf) {
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+}
